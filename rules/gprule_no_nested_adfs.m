bfc08ce6be6b56dab133519adda6565fe69ebaff
AlekseiTepljakov <alex@starspirals.net>
2022-06-10 17:59:45 +0300

    Add max nest level to ev. rule no_nested_adfs

--- rules/gprule_no_nested_adfs.m
+++ rules/gprule_no_nested_adfs.m
@@ -1,12 +1,26 @@
 function [gp_out, fit] = gprule_no_nested_adfs(gp, expr, params)
 %GPRULE_NO_NESTED_ADFS Individuals with nested ADFs are discarded
 %   Individuals having the trait of being expressed through nested ADFs
-%   should not have any chances of survival.
+%   with a max_nesting_level greater than allowed should be discarded.
+%
+% Example configuration:
+%
+% % Max nesting level config
+% op = struct;
+% op.max_nesting_level = 2;
+%
+% % General settings
+% gp.evolution.rules.use = true;
+% gp.evolution.rules.sets = {{@gprule_no_nested_adfs, op}};
+% gp.evolution.rules.attempts = 500;
 
 gp_out = gp;
 
-% Params are not used at the moment
-params; %#ok
+% Maximum nesting level
+max_nesting_level = 1;
+if isfield(params, 'max_nesting_level')
+    max_nesting_level = params.max_nesting_level;
+end
 
 % In expressions, ADFs are always encoded as certain known symbols
 adfs = gp.nodes.adf.seed_str;
@@ -20,10 +34,25 @@ fit = 1.0; % By default, the gene is fit for survival
 % Here we assume that some rules for expression strings hold,
 % e.g., that they are always complete [TODO: potential bug]
 while k <= length(expr)
+
+%     % DEBUG
+%     disp('Have the following expression:');
+%     disp(expr);
+%     disp('Have the following ADFs:');
+%     disp(gp.nodes.adf.seed_str);
     
     if ~isempty(strfind(adfs, expr(k))) %#ok: backwards compatibility
-        if adf_found % Nesting detected
+        if adf_found && open_count >= max_nesting_level % Nesting detected
             fit = 0.0;
+%             % DEBUG
+%             fprintf(2, 'Found %d level nesting! Discarding invidivual.\n', open_count);
+% 
+%             disp('Have the following expression:');
+%             disp(expr);
+%             disp('Have the following ADFs:');
+%             disp(gp.nodes.adf.seed_str);
+% 
+%             pause;
             break;
         else
             adf_found = true;
